ac2226c7f1280149b25db3867f50107d4f8b1d6d
Roberto <roberto.quadraccia90@gmail.com>
2023-10-03 12:23:15 +0200

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Roberto Quadraccia on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
